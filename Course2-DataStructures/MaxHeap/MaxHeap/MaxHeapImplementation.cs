17012d5f4fdf24478d7bd71c0841953012adf1e5
Rashmi <geminirashmi31@gmail.com>
2019-07-16 22:11:21 -0700

    Max Heap

---
+++ Course2-DataStructures/MaxHeap/MaxHeap/MaxHeapImplementation.cs
@@ -0,0 +1,196 @@
+﻿using System;
+
+namespace MaxHeap
+{
+    public abstract class HeapImplementation
+    {
+        private int Capacity { get; set; }
+        internal int Size { get; set; }
+        internal int[] Arr { get; set; }
+
+        public HeapImplementation()
+        {
+            Capacity = 100;
+            Size = 0;
+            Arr = new int[Capacity];
+        }
+
+        public void EnlargeIfNeeded()
+        {
+            if (Size == Capacity)
+            {
+                Capacity = 2 * Capacity;
+                Array.Copy(Arr, Arr, Capacity);
+            }
+        }
+
+        public int GetLeftChildIndex(int parentIndex)
+        {
+            return 2 * parentIndex + 1;
+        }
+
+        public bool HasLeftChild(int parentIndex)
+        {
+            return GetLeftChildIndex(parentIndex) < Size;
+        }
+
+        public int LeftChild(int index)
+        {
+            return Arr[GetLeftChildIndex(index)];
+        }
+
+        public int GetRightChildIndex(int parentIndex)
+        {
+            return 2 * parentIndex + 2;
+        }
+
+        public bool HasRightChild(int parentIndex)
+        {
+            return GetRightChildIndex(parentIndex) < Size;
+        }
+
+        public int RightChild(int index)
+        {
+            return Arr[GetRightChildIndex(index)];
+        }
+
+        public int GetParentIndex(int childIndex)
+        {
+            return (childIndex - 1) / 2;
+        }
+
+        public bool HasParent(int childIndex)
+        {
+            return GetParentIndex(childIndex) >= 0;
+        }
+
+        public int Parent(int index)
+        {
+            return Arr[GetParentIndex(index)];
+        }
+
+        public void Swap(int index1, int index2)
+        {
+            int temp = Arr[index1];
+            Arr[index1] = Arr[index2];
+            Arr[index2] = temp;
+        }
+
+        public int peek()
+        {
+            if (Size == 0)
+                throw new InvalidOperationException("Heap is empty");
+
+            return Arr[0];
+        }
+
+        public int Pop()
+        {
+            if (Size == 0)
+                throw new InvalidOperationException("Heap is empty");
+
+            int item = Arr[0];
+            Arr[0] = Arr[Size - 1];
+            Size--;
+            HeapifyDown();
+            return item;
+        }
+
+        public void Add(int item)
+        {
+            EnlargeIfNeeded();
+            Arr[Size] = item;
+            Size++;
+            HeapifyUp();
+        }
+
+        internal abstract void HeapifyUp();
+        internal abstract void HeapifyDown();
+    }
+
+    public class MaxHeapDerived : HeapImplementation
+    {
+
+        public MaxHeapDerived() : base()
+         {
+            
+          }
+       
+        internal override void HeapifyDown()
+        {
+            int index = 0;
+            while (HasLeftChild(index))
+            {
+                int LargerChildIndex = GetLeftChildIndex(index);
+                if (HasRightChild(index) && RightChild(index) > LeftChild(index))
+                {
+                    LargerChildIndex = GetRightChildIndex(index);
+                }
+
+                if (Arr[LargerChildIndex] > Arr[index])
+                    Swap(index, LargerChildIndex);
+                else
+                    break;
+                index = LargerChildIndex;
+            }
+        }
+
+        internal override void HeapifyUp()
+        {
+            int index = Size - 1;
+
+            while (HasParent(index) && Parent(index) < Arr[index])
+            {
+                Swap(index, GetParentIndex(index));
+                index = GetParentIndex(index);
+            }
+        }
+
+        public int ExtractMax()
+        {
+            int result = Arr[0];
+            Arr[0] = Arr[Size - 1];
+            Size = Size - 1;
+            HeapifyDown();
+            return result;
+        }
+    }
+
+
+    public class MinHeap : HeapImplementation
+    {
+        public MinHeap() : base()
+        {
+        }
+       
+        internal override void HeapifyDown()
+        {
+            int index = 0;
+            while (HasLeftChild(index))
+            {
+                int smallerChildIndex = GetLeftChildIndex(index);
+                if (HasRightChild(index) && RightChild(index) < LeftChild(index))
+                {
+                    smallerChildIndex = GetRightChildIndex(index);
+                }
+
+                if (Arr[smallerChildIndex] < Arr[index])
+                    Swap(index, smallerChildIndex);
+                else
+                    break;
+                index = smallerChildIndex;
+            }
+        }
+
+        internal override void HeapifyUp()
+        {
+            int index = Size - 1;
+
+            while (HasParent(index) && Parent(index) > Arr[index])
+            {
+                Swap(index, GetParentIndex(index));
+                index = GetParentIndex(index);
+            }
+        }
+    }
+}
---
+++ Course2-DataStructures/MaxHeap/MaxHeap/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+
+namespace MaxHeap
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            /*MaxHeaps mh = new MaxHeaps(8);
+            mh.Insert(2);
+            mh.Insert(6);
+            mh.Insert(5);
+            mh.Insert(4);
+            mh.Insert(1);
+            int res =  mh.ExtractMax();
+            Console.WriteLine("result {0}", res);*/
+
+            MaxHeapDerived mhp = new MaxHeapDerived();
+            mhp.Add(2);
+            mhp.Add(6);
+            mhp.Add(5);
+            mhp.Add(4);
+            mhp.Add(1);
+
+            int result = mhp.Pop();
+            Console.WriteLine("result {0}", result);
+
+            MinHeap minH = new MinHeap();
+            minH.Add(2);
+            minH.Add(6);
+            minH.Add(5);
+            minH.Add(4);
+            minH.Add(1);
+
+            int minHeapResult = minH.Pop();
+            Console.WriteLine("Minheap result {0}", minHeapResult);
+
+        }
+    }
+}
+
+
+   /* public class MaxHeaps
+    {
+        int[] Array { get; set; }
+        int heapSize;
+        int capacity;
+
+        public MaxHeaps(int capacity)
+        {
+            this.heapSize = 0;
+            this.Array = new int[capacity];
+            this.capacity = capacity;
+        }
+
+        private int Parent(int i)
+        {
+            return (i - 1) / 2;
+        }
+
+        private int LeftChild(int i)
+        {
+            return (2 * i + 1);
+        }
+
+        private int RightChild(int i)
+        {
+            return (2 * i + 2);
+        }
+
+        // TODO: Check if swap is effective for array
+        private void Swap(int i, int j)
+        {
+            int temp = Array[i];
+            Array[i] = Array[j];
+            Array[j] = temp;
+        }
+
+
+        private void Siftdown(int i)
+        {
+            int index = 0;
+            while (LeftChild(i))
+            {
+                int largerChildIndex = getLeftChildIndex(index);
+                if (hasRightChild(index) && rightChild(index) > leftChild(index))
+                {
+                    largerChildIndex = getRightChildIndex(index);
+                }
+
+                if (Nodes[largerChildIndex] > Nodes[index])
+                    swap(index, largerChildIndex);
+                else
+                    break;
+                index = largerChildIndex;
+            }
+            int maxIndex = i;
+            int l = LeftChild(i);
+            int r = RightChild(i);
+
+            if ((l <= heapSize) && (Array[l] > Array[maxIndex]))
+            {
+                maxIndex = l;
+            }
+            else
+            {
+                maxIndex = i;
+            }
+
+            if ((r <= heapSize) && (Array[r] > Array[maxIndex]))
+            {
+                maxIndex = r;
+            }
+
+            if (i != maxIndex)
+            {
+                Swap(i, maxIndex);
+                Siftdown(maxIndex);
+            }
+
+            //Console.WriteLine(maxIndex);
+        }
+
+        private void Siftup(int i)
+        {
+            i = heapSize - 1;
+            while ((i > 0) && (Array[Parent(i)] < Array[i]))
+            {
+                Swap(Array[i], Array[Parent(i)]);
+                i = Parent(i);
+            }
+
+        }
+
+        public int ExtractMax()
+        {
+            int result = Array[0];
+            Array[0] = Array[heapSize -1];
+            heapSize = heapSize - 1;
+            Siftdown(0);
+            return result;
+        }
+
+        public void Insert(int p)
+        {
+            if (heapSize == capacity)
+            {
+                return;
+            }
+
+            heapSize++;
+            Array[heapSize - 1] = p;
+            Siftup(heapSize);
+             
+        }
+
+        public void Remove(int i)
+        {
+            Array[i] = -1;
+            Siftup(i);
+            ExtractMax();
+        }
+
+        private void ChangePriority(int i, int p)
+        {
+            int oldp;
+            oldp = Array[i];
+            Array[i] = p;
+            if(p > oldp)
+            {
+                Siftup(i);
+            }
+            else
+            {
+                Siftdown(i);
+            }
+        }*/
+    
+
+
