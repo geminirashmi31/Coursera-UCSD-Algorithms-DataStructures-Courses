137124dcb49be53e3c3c814b554c8ad4efcf0f45
Rashmi <geminirashmi31@gmail.com>
2019-07-16 21:39:30 -0700

    Least Common Multiple

---
+++ Course1-AlgorithmicToolbox/lcm/lcm/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace lcm
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			string[] numStr = Console.ReadLine().Split(' ');
+			long[] nums = new long[numStr.Length];
+
+			for (long i = 0; i < numStr.Length; i++)
+			{
+                nums[i] = Convert.ToInt64(numStr[i]);
+			}
+
+            long num1 = nums[0];
+            long num2 = nums[1];
+
+            //int result = Gcd(num1, num2);
+            long result = Lcm(num1, num2);
+			Console.WriteLine(result);
+		}
+
+        public static long Gcd(long a, long b)
+		{
+            long res;
+			if (b == 0)
+			{
+				return a;
+			}
+
+            long rem = a % b;
+			if (rem > b)
+			{
+				res = Gcd(rem, b);
+			}
+			else
+			{
+				res = Gcd(b, rem);
+			}
+
+			return res;
+		}
+
+        public static long Lcm(long a, long b)
+        {
+            long result = Gcd(a, b);
+            long lcm = (a * b) / result;
+            return lcm;
+        }
+	}
+}
+
