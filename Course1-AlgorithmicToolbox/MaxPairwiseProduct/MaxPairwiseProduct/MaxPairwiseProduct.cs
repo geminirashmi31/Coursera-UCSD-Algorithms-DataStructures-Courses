e2ac30d069cb3e11c93355b357a521f48e5a7bdd
Rashmi <geminirashmi31@gmail.com>
2019-07-16 21:19:30 -0700

    Max Pairwise Product

---
+++ Course1-AlgorithmicToolbox/MaxPairwiseProduct/MaxPairwiseProduct/MaxPairwiseProduct.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MaxPairwiseProduct
+{
+    class MaxPairwiseProduct
+    {
+        public static void Main(string[] args)
+        {
+            long size = Convert.ToInt32(Console.ReadLine());
+            long [] num = new long [size];
+
+            string[] numArr = Console.ReadLine().Split(' ');
+
+
+            for (long k = 0; k < numArr.Length; k++)
+            {
+                num[k] = Convert.ToInt64(numArr[k]);
+            }
+
+            long proResult = getMaxPairwiseProduct(num);
+            Console.WriteLine(proResult);
+
+        }
+
+        public static long getMaxPairwiseProduct(long[] arr)
+        {
+			int max_index1 = -1;
+            for (int i = 0; i < arr.Length; i++)
+				if ((max_index1 == -1) || (arr[i] > arr[max_index1]))
+					max_index1 = i;
+
+			int max_index2 = -1;
+            for (int j = 0; j < arr.Length; ++j)
+				if ((j != max_index1) && ((max_index2 == -1) || (arr[j] > arr[max_index2])))
+					max_index2 = j;
+
+			return ((long)(arr[max_index1])) *arr[max_index2];
+        }
+    }
+}
+
---
+++ Course1-AlgorithmicToolbox/MaxPairwiseProduct/MaxPairwiseProduct/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MaxPairwiseProduct
+{
+    class MaxPairwiseProduct
+    {
+        public static void Main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] num = new int[size];
+
+            string[] numArr = Console.ReadLine().Split(' ');
+
+
+            for (int k = 0; k < numArr.Length; k++)
+            {
+                num[k] = Convert.ToInt32(numArr[k]);
+            }
+
+            int proResult = getMaxPairwiseProduct(num);
+            Console.WriteLine(proResult);
+
+        }
+
+        public static int getMaxPairwiseProduct(int[] arr)
+        {
+            int result = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] * arr[j] > result)
+                    {
+                        result = arr[i] * arr[j];
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
+
